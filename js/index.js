38acb79bbe17dff98438793af7d47117cc865dfd
cladams <cladams0203@gmail.com>
2019-05-22 17:57:52 -0400

    halfway through adding a popup login screen

--- js/index.js
+++ js/index.js
@@ -47,6 +47,49 @@ Array.from(tags).forEach((btn) => {
   return new Cursor(btn);
 });
 
+// popup box
+
+class Popup {
+  constructor (element){
+    this.element = element;
+    console.log(this.element);
+    //create html elements
+    this.popContent = document.createElement('div');
+    this.closeDiv = document.createElement('div')
+    this.close = document.createElement('span');
+    this.login = document.createElement('form');
+    this.uName = document.createElement('input');
+    this.uPass = document.createElement('input');
+    this.submit = document.createElement('button');
+    
+    //add classes
+    this.element.classList.add('pop-contain');
+    this.popContent.classList.add('pop-content');
+    this.close.classList.add('close');
+    this.closeDiv.classList.add('close-div');
+    this.login.classList.add('form');
+    //text content for form
+    this.close.textContent = 'x';
+    this.textUser = 'User Name :  ';
+    this.textPass = 'Password :  ';
+    //insert elements
+    this.element.append(this.popContent);
+    this.popContent.append(this.closeDiv);
+    this.closeDiv.append(this.close);
+    this.popContent.append(this.login);
+    this.login.append(this.textUser);
+    this.login.append(this.uName);
+    this.login.append(this.textPass);
+    this.login.append(this.uPass);
+  }
+  
+}
+const popup = document.querySelector('.popup');
+document.querySelector('.login').addEventListener('click', function() {
+  const popup = document.querySelector('.popup');
+  return new Popup(popup);
+});
+
 
 
 
